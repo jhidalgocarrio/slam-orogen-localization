b58765ad3180df21d8af640ddfcc4b3b3917d89c
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2013-01-16 11:23:44 +0100

    corrected transformer transformation from head_vicon to body

--- config/transforms.rb
+++ config/transforms.rb
@@ -4,8 +4,9 @@ static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.0
     Eigen::Vector3.new( 0.01302, -0.0654, 0.02794 ),
     "stim300" => "body"
 
-static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI - 8.33 * (Math::PI/180.00), -14.034 * (Math::PI/180.00), 3.969 * (Math::PI/180.00)), 2,1,0),
-    Eigen::Vector3.new( 0.09, -0.155, 0.195 ),
+#In this tranformation the translation is after performing the rotation
+static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.034 * (Math::PI/180.00), 3.9525 * (Math::PI/180.00)), 2,1,0),
+    Eigen::Vector3.new( 0.23, 0.0875, -0.21 ),
     "vicon_head" => "body"
 
 static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(4.0 * (Math::PI/180.00), 0, 0 ), 2,1,0),
