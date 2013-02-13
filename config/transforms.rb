60ac5736889ce656585f8eae34d6b8fb8980d96a
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2013-02-13 22:35:03 +0100

    Zero in roll for vicon_head => body transformation

--- config/transforms.rb
+++ config/transforms.rb
@@ -5,10 +5,12 @@ static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.0
     "stim300" => "body"
 
 #In this tranformation the translation is after performing the rotation
-static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.034 * (Math::PI/180.00), 3.9525 * (Math::PI/180.00)), 2,1,0),
-    Eigen::Vector3.new( 0.23, 0.0875, -0.21 ),
+# static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.034 * (Math::PI/180.00), 3.9525 * (Math::PI/180.00)), 2,1,0),
+static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.034 * (Math::PI/180.00), 0.00), 2,1,0),
+    Eigen::Vector3.new( 0.1406, 0.075, -0.21 ),
     "vicon_head" => "body"
 
+#In this tranformation the translation is after performing the rotation
 static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(4.0 * (Math::PI/180.00), 0, 0 ), 2,1,0),
     Eigen::Vector3.new( 0.0, 0.0, 0.0 ),
     "vicon_body" => "body"
