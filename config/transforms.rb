f01601855b6addb3f0323378f4600075c774d1df
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2013-10-10 14:23:34 +0200

    Update Ruby files

--- config/transforms.rb
+++ config/transforms.rb
@@ -1,6 +1,12 @@
 require 'eigen'
 
-#Transformation the translation is after performing the rotation (vbody = Tbody_stim300 * vstim300)
+# Transformation the translation is after performing the rotation (vbody = Tbody_stim300 * vstim300)
+# The convention to follow is "frame1" => "frame2" means the base of frame2 is expressed in the based of frame1
+# Therefore Tstim300_2_body is the result of the transformation.
+# Keep in mind that if we have a vector in stim300-frame and we want to have it in body-frame.
+# v_body = (Tstim300_2_body)^-1 v_stim300 which is v_body = Tbody_2_stim300 v_stim300
+
+
 #static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI + (2.0*(Math::PI/180.00)), -14.654 * (Math::PI/180.00), 0 ), 2,1,0),
 #static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -15.3 * (Math::PI/180.00), 0), 2,1,0),
 static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new(Math::PI, -14.034 * (Math::PI/180.00), 0 ), 2,1,0),
