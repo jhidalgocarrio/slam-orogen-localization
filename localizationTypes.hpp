237221aa6d46e7c5dadc3dc013fd6c91c1465ca8
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-07-18 20:12:32 +0200

    task and orogen: removed dispatcher types: removed type for configuration of dynamic ports

--- localizationTypes.hpp
+++ localizationTypes.hpp
@@ -13,23 +13,6 @@
 
 namespace localization
 {
-    struct OutputPortsConfiguration
-    {
-        /** Name of the input port */
-        std::string output_name;
-
-        /**  This is a five element vector **/
-
-        /* Names of the input index. Index previous to next feature. **/
-        std::string index_name;
-
-        /* Names of the input covariance. Inaccuracies in the displacement-estimation. **/
-        std::string pointcloud_name;
-
-        /* Names of the input covariance. Inaccuracies in the displacement-estimation. **/
-        std::string covariance_name;
-    };
-
     struct Feature
     {
         boost::uuids::uuid index; // Indexes of the points/samples uses to compute the relative measurement
